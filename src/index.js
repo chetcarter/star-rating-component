98d687232b590de1f005bdcd6f903e50cffdda55
Chet Carter <abazabalv@gmail.com>
2021-01-28 14:05:32 -0700

    Final touches

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { FaStar } from 'react-icons/fa';
+
+const createArray = (length) => [
+  ...Array(length)
+];
+
+function Star({ selected = false, onSelect }) {
+  return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />
+}
+
+function StarRating({ totalStars = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)} />
+      ))}
+      <p>{selectedStars} of {totalStars}</p>
+    </>
+  );
+}
+
+function App() {
+  return <StarRating totalStars={10}/>;
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +33,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
